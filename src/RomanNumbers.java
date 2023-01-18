62aa13aa428a5b39c2d16ee574af56dc985261f4
Adam <ahollywade@gmail.com>
2023-01-18 23:00:17 +0300

    1.0

---
+++ src/RomanNumbers.java
@@ -0,0 +1,29 @@
+public class RomanNumbers {
+
+    static final String[] ROMAN_NUMBERS = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+            "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",
+            "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",
+            "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",
+            "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX",
+            "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",
+            "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
+    public static int romanToArab (String str) {
+        int c = -1;
+        for (int i = 0; i < 100; i++) {
+            if (ROMAN_NUMBERS[i].equals(str)) {
+                c = i;
+            }
+        }
+        return c;
+    }
+
+    public static StringBuilder arabToRoman (int arab) {
+        StringBuilder arabNumber = new StringBuilder();
+        for (int i = 0; i < 100; i++) {
+            if (i == arab) {
+                arabNumber.append(ROMAN_NUMBERS[i]);
+            }
+        }
+        return arabNumber;
+    }
+}
